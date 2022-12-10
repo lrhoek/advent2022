c3b361c8c18e3ea42e2d4fdd924f477d8f2e4cef
Laurens Hoek <lrhoek@gmail.com>
2022-12-10 21:42:58 +0100

    Day 10 alternative solution cleanup

--- 10/10.alt.php
+++ 10/10.alt.php
@@ -1,18 +1,38 @@
 <?php
 
-function execute($state, $instruction) {
-    return array_reduce(array_map(intval(...), explode(" ", $instruction)), cycle(...), $state);
-}
+function init() {
+    $state["line"] = [];
+    $state["strength"] = [];
+    $state["x"] = [1];
+    $state["crt"] = array_fill(0, 40, '.');
 
-function cycle($state, $delta = 0) {
-    $x = end($state["x"]);
-    $position = (count($state["x"]) - 1) % 40;
-    $state["crt"][] = [...end($state["crt"]), $position => in_array($position, range($x - 1, $x + 1)) ? "#" : "."];
-    $state["x"][] = $x + $delta;
     return $state;
 }
 
-$state = array_reduce(explode(PHP_EOL, file_get_contents('input')), execute(...), ["x" => [1], "crt" => [array_fill(0, 40, '.')]]);
+function execute($state, $instruction) {
+    $deltas = array_map(intval(...), explode(" ", $instruction));
+    return array_reduce($deltas, cycle(...), $state);
+}
 
-echo array_sum(array_map(fn ($cycle) => $cycle * $state["x"][$cycle-1], range(20, count($state["x"]), 40))).PHP_EOL;
-echo join(PHP_EOL, array_map(fn ($cycle) => join("", $state["crt"][$cycle]), range(40, count($state["x"]), 40))).PHP_EOL;
+function cycle($state, $delta) {
+    $x = end($state["x"]);
+    $cycle = count($state["x"]);
+
+    $position = ($cycle - 1) % 40;
+    $strength = ($cycle - 20) % 40 === 0 ? [$cycle => $cycle * $x] : [];
+    $line = ($cycle + 1) % 40 === 0 ? [join("", $state["crt"])] : [];
+    $pixel = in_array($position, range($x - 1, $x + 1)) ? "#" : ".";
+
+    $state["crt"][$position] = $pixel;
+    $state["x"][] = $x + $delta;
+    array_push($state["strength"], ...$strength);
+    array_push($state["line"], ...$line);
+
+    return $state;
+}
+
+$instructions = explode(PHP_EOL, file_get_contents('input'));
+$state = array_reduce($instructions, execute(...), init());
+
+echo array_sum($state["strength"]).PHP_EOL;
+echo join(PHP_EOL, $state["line"]).PHP_EOL;
